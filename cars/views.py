2b994d68c5638fa744bc6fa49ed692bd49bfc6f6
Carlos Junior <96848712+jccarlosjr@users.noreply.github.com>
2024-05-07 14:19:05 -0300

    updt

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
+from django.views.generic import ListView, CreateView, DetailView
 
 
 # def car_view(request):
@@ -10,6 +9,29 @@ from django.views import View
 #     if search:
 #         cars = cars.filter(model__icontains=search)
 #     return render(request, 'cars.html', {'cars': cars})
+
+
+# class CarView(View):
+
+#     def get(self, request):
+#         cars = Car.objects.all().order_by('-model')
+#         search = request.GET.get('search')
+#         if search:
+#             cars = cars.filter(model__icontains=search)
+#         return render(request, 'cars.html', {'cars': cars})
+
+
+class CarListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
 
 
 # def new_car_view(request):
@@ -23,25 +45,27 @@ from django.views import View
 #     return render(request, 'new_car.html', { 'new_car_form': new_car_form})
 
 
-class CarView(View):
-
-    def get(self, request):
-        cars = Car.objects.all().order_by('-model')
-        search = request.GET.get('search')
-        if search:
-            cars = cars.filter(model__icontains=search)
-        return render(request, 'cars.html', {'cars': cars})
-
-
-class NewCarView(View):
+# class NewCarView(View):
     
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form})
+#     def get(self, request):
+#         new_car_form = CarModelForm()
+#         return render(request, 'new_car.html', { 'new_car_form': new_car_form})
     
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form})
+#     def post(self, request):
+#         new_car_form = CarModelForm(request.POST, request.FILES)
+#         if new_car_form.is_valid():
+#             new_car_form.save()
+#             return redirect('cars_list')
+#         return render(request, 'new_car.html', { 'new_car_form': new_car_form})
+
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
